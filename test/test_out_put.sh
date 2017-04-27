f7bb392cb672c67aaa3c8727d2504873194247bc
cn <cool_navy@qq.com>
2017-04-27 12:54:09 +0800

    update test_out_put.sh

--- test/test_out_put.sh
+++ test/test_out_put.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+# 测试转出 lua 表所有字段是否合法
+
 set -e
 
 fin=$1
@@ -10,6 +12,8 @@ for f in $(ls *.lua);do
 	tname=${f/\.*/}
 	lua -e "t${tname}=require \"${tname}\";for kk, vv in pairs(t${tname}) do for k,v in pairs(t${tname}.head)do print(\"${tname}[\"..kk..\"].\"..k..\" = \".. vv[k]) end end" 1> /dev/null
 	if [[ $? = "0" ]]; then
-	 	echo "$f test passed"
-	 fi 
+		echo "$f test passed"
+	else
+		echo "$f error occurred"
+	fi 
 done
